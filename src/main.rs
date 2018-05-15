a10e1645022215b2a5badf7ef2cb0c3ff349e00c
Cedric Gatay <c.gatay@code-troopers.com>
2018-05-15 08:34:30 +0200

    feat(rust): Handle not existing command
    Extracted config struct
    Handle a not existing command to execute straight away

--- src/main.rs
+++ src/main.rs
@@ -12,44 +12,54 @@ use regex::Regex;
 fn main() {
     println!("====== {:?} =========", env::args());
     let app_args: Vec<String> = env::args().collect();
+    let config = Config::new(app_args);
 
-    let (query, mut rest_args) = app_args.split_at(2);
-
-    println!("{:?}, {:?}", query, rest_args);
-    println!("Searching for {}", query[1]);
-
+    println!("{:?}, {:?}", config.command, config.args);
 
     let mut f = File::open(".ctproject").expect("file not found");
 
     let mut contents = String::new();
     f.read_to_string(&mut contents)
         .expect("something went wrong reading the file");
-    println!("{}",contents);
 
-    let matching_line = contents.lines()
-        .filter(|line| line.contains(&query[1]))
-        .last().unwrap();
+    let matching_line: String = contents.lines()
+        .filter(|line| line.contains(&config.command))
+        .last()
+        //build a "fake" command with the one the user tries to execute
+        .map_or(format!("{}={}", &config.command, &config.command), |v| { v.to_string() });
 
 
     println!("Matching line:\n{}", matching_line);
     let regex = Regex::new(r"^[^=]*=([^#]*)#?(.*)").unwrap();
-    for capture in regex.captures_iter(matching_line){
+    for capture in regex.captures_iter(&matching_line){
         let command_with_args = &capture[1].replace("\"", "");
         let doc = &capture[2];
-        println!("{}, {}", command_with_args, doc);
+        println!("Command with args  {} and doc {}", command_with_args, doc);
         let commands_vec: Vec<_> = command_with_args.split(" ").collect();
-        let (command, mut args) = commands_vec.split_first().unwrap();
+        let (command, args) = commands_vec.split_first().unwrap();
 
         let mut args_as_vect: Vec<String> = args.iter().map(|s| s.to_string()).collect();
-        args_as_vect.append(rest_args.to_vec().as_mut());
+        args_as_vect.append(&mut config.args.clone());
 
-        println!("{:?} {:?}", args, args_as_vect);
+        println!("Args : {:?} {:?}", args, args_as_vect);
         let mut s = Command::new(command)
             .args(args_as_vect)
             .spawn().unwrap();
-        let output = s.wait_with_output();
-        println!("{:?}", output.unwrap().stdout);
+        //result printed to stdout / stderr as expected as io are shared
+        let _output = s.wait_with_output();
+        // println!(">> {:?}", output.unwrap().stdout);
 
     }
 
+}
+
+struct Config{
+    command: String,
+    args: Vec<String>,
+}
+impl Config{
+    fn new(args: Vec<String>) -> Config{
+        let (query, rest_args) = args.split_at(2);
+        Config { command: query[1].to_owned(), args: rest_args.to_vec() }
+    }
 }
