83e7b6ae2d6129ac968a1ccebcf9525a52ef8313
Cedric Gatay <c.gatay@code-troopers.com>
2018-06-19 19:08:21 +0200

    chore(): remove dead code
    find_command was never used.

--- src/lib.rs
+++ src/lib.rs
@@ -36,17 +36,7 @@ use hyper::rt::Future;
 use hyper::service::service_fn;
 use hyper::{Body, Method, Request, Response, Server, StatusCode, HeaderMap};
 use hyper::header::CONTENT_TYPE;
-use cli::Config;
 
-pub fn find_command(config: &Config, ct_file: &CTFile) -> String{
-    println!("{}", ct_file.content);
-    let matching_line: String = ct_file.content.lines()
-        .filter(|line| line.starts_with(&config.command))
-        .last()
-        //build a "fake" command with the one the user tries to execute
-        .map_or(format!("{}={}", &config.command, &config.command), |v| { v.to_string() });
-    matching_line
-}
 
 pub fn show_banner(){
     let show_banner = env::var("CTNOBANNER").unwrap_or("false".to_string());
